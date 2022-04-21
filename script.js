692fa5208becf559e25db3382af62d00845ccd6c
Milagros Gauna <milagrosgauna30@gmail.com>
2022-04-20 23:51:35 -0300

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+let nombreApellido = prompt("Nombre y apellido de quien reserva");
+if (nombreApellido == ""){
+    alert ("No olvides ingresar tu nombre y apellido");
+}
+else {
+    alert ("Reserva:" + " " + nombreApellido)
+}
+
+
+let diaMes = parseInt(prompt("Dia y mes en el que le gustaria visitarnos"));
+if (diaMes == ""){
+    alert ("No olvides ingresar el dia y mes");
+}
+else{
+    alert ("Dia:" + " " + diaMes)
+}
+
+
+
