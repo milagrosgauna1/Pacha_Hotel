f523ffc1032c3ded299bf9f2f47bbb60b7b7aa9a
Milagros Gauna <milagrosgauna30@gmail.com>
2022-04-27 22:50:16 -0300

    scss1

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ else{
     alert ("Dia:" + " " + diaMes)
 }
 
+
+function Producto(nombre, capacidad, precio, stock){
+    this.nombre = nombre
+    this.capacidad = capacidad
+    this.precio = precio
+    this.stock = stock
+}
+
+const producto1 = new Producto("Habitacion Estandar", 2, 70, 5)
+const producto2 = new Producto("Habitacion Suite", 4, 110, 5)
+const producto3 = new Producto("Habitacion Micro", 2, 50, 5)
+const producto4 = new Producto("Habitacion Familiar", 5, 100, 3)
+const producto5 = new Producto("Habitacion Comaprtida", 6, 40, 10)
+
+console.log(producto1)
+
+let array = [Producto]
+console.log(array)
+
+const miArray = [Producto]
+console.log (myArray.length);
